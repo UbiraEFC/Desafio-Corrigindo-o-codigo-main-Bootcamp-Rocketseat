caf8e1293f99ebe69db4f16322bf9df17739d27a
UbiraEFC <ubirata.efc@gmail.com>
2022-08-09 07:37:29 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  if (title) repository.title = title;
+  if (url) repository.url = url;
+  if (techs) repository.techs = techs;
+
+  repositories.push(repository);
+
+
+  return response.status(200).json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositories.indexOf(repository), 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repository.likes++
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
+});
+
+module.exports = app;
